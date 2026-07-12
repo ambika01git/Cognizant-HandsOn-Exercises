57996cdc224aeb56e368cfaa535e1285cfbbafbb
ambika01git <ambikamishra2345@gmail.com>
2026-07-12 20:11:29 +0530

    Add files via upload

---
+++ Spring-Core-and-Maven/LibraryManagementSpring/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,21 @@
+//package com.library.repository;
+//
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public class BookRepository {
+//
+//    public void display() {
+//        System.out.println("BookRepository method called.");
+//    }
+//
+//}
+package com.library.repository;
+
+public class BookRepository {
+
+    public void display() {
+        System.out.println("BookRepository method called.");
+    }
+
+}
